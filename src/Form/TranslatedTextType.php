058ff5aa73dbab02f28802c5e8f471bbb01f883a
valiclud <valicek@atlas.cz>
2023-12-28 14:03:26 +0100

    Rearrangement

--- src/Form/TranslatedTextType.php
+++ src/Form/TranslatedTextType.php
@@ -4,15 +4,21 @@ namespace App\Form;
 
 use App\Entity\OriginalText;
 use App\Entity\TranslatedText;
+use App\Entity\Place;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
+
 
 class TranslatedTextType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+
+
         $builder
             ->add('author')
             ->add('title')
@@ -22,9 +28,9 @@ class TranslatedTextType extends AbstractType
             ->add('revision')
             ->add('originalText', EntityType::class, [
                 'class' => OriginalText::class,
-'choice_label' => 'id',
+                'choice_label' => 'title',
             ])
-        ;
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
