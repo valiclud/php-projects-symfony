802aa3b681d5c83eefda1ac931aaf98e40266b20
valiclud <valicek@atlas.cz>
2023-12-28 10:59:55 +0100

    Improving Form Login to the application.

--- src/Controller/OriginalTextController.php
+++ src/Controller/OriginalTextController.php
@@ -12,7 +12,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+//#[IsGranted('ROLE_ADMIN')]
 #[Route('/originaltext')]
 class OriginalTextController extends AbstractController
 {
